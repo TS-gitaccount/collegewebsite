5178f3adf388d9fbfbe74e04cd31a6d5914726f2
Tamil Selvi <tamilselvisivakumar2004@gmail.com>
2024-10-18 20:46:13 +0530

    Update formhanding.php

--- codding/formhanding.php
+++ codding/formhanding.php
@@ -4,7 +4,7 @@ $visitor_email=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
 
- $email_from='info@yourwebsite.com';  //domain name
+ $email_from='nbidc.weebly.com';  //domain name
 
 $email_subject='New Form Submission';
 
