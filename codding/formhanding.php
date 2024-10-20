fe5312d22b04eb4121e8647ac0b2435967b2d408
Tamil Selvi <tamilselvisivakumar2004@gmail.com>
2024-10-20 22:09:18 +0530

    Update formhanding.php

--- codding/formhanding.php
+++ codding/formhanding.php
@@ -1,51 +1,51 @@
-<?php
-$name = htmlspecialchars($_POST['name']);
-$email=htmlspecialchars($_POST['email']);
-$subject=htmlspecialchars($_POST['subject']);
-$message=htmlspecialchars($_POST['message']);
+// <?php
+// $name = htmlspecialchars($_POST['name']);
+// $email=htmlspecialchars($_POST['email']);
+// $subject=htmlspecialchars($_POST['subject']);
+// $message=htmlspecialchars($_POST['message']);
 
-if (!empty($name) && !empty($email) && !empty($subject)&& !empty($message)) {
+// if (!empty($name) && !empty($email) && !empty($subject)&& !empty($message)) {
         
-        echo "Thank you for your feedback, $name!<br>";
-        echo "Email: $email<br>";
+//         echo "Thank you for your feedback, $name!<br>";
+//         echo "Email: $email<br>";
 
 
-  $to = "charu18092005@gmail.com"; // Change this to your email
-        $subject = "New Feedback from $name";
-        $mess = "Name: $name\nEmail: $email\nMessae:\n$message";
-        $headers = "From: $email";
+//   $to = "charu18092005@gmail.com"; // Change this to your email
+//         $subject = "New Feedback from $name";
+//         $mess = "Name: $name\nEmail: $email\nMessae:\n$message";
+//         $headers = "From: $email";
 
-        if (mail($to, $subject, $message, $headers)) {
-            echo "<br>Feedback has been sent via email.";
-        } else {
-            echo "<br>Sorry, there was an issue sending your feedback.";
-        }
+//         if (mail($to, $subject, $message, $headers)) {
+//             echo "<br>Feedback has been sent via email.";
+//         } else {
+//             echo "<br>Sorry, there was an issue sending your feedback.";
+//         }
 
-    } else {
-        echo "All fields are required.";
-    }
-} else {
-    echo "Invalid Request.";
-}
-?>
+//     } else {
+//         echo "All fields are required.";
+//     }
+// } else {
+//     echo "Invalid Request.";
+// }
+// ?>
 
-<!--  $email_from=$_POST['email'];  //domain name
+// <!--  $email_from=$_POST['email'];  //domain name
 
-$email_subject='New Form Submission';
+// $email_subject='New Form Submission';
 
-$email_body=" User Name : $name .\n".
-            "User Email: $visitor_email.\n".
-            "Subject: $subject.\n".
-            "User Message: $message .\n";
+// $email_body=" User Name : $name .\n".
+//             "User Email: $visitor_email.\n".
+//             "Subject: $subject.\n".
+//             "User Message: $message .\n";
 
-$to='charu18092005@gmail.com'; //your email to store
+// $to='charu18092005@gmail.com'; //your email to store
 
-$headers="From: $email_from \r\n";
+// $headers="From: $email_from \r\n";
 
-$headers .= "Reply-To : $visitor_email \r\n";
+// $headers .= "Reply-To : $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+// mail($to,$email_subject,$email_body,$headers);
 
-header("location: contact.html")
-?> 
- -->
+// header("location: contact.html")
+// ?> 
+//  -->
